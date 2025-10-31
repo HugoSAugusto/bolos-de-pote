bce3531f9f1c78ad0bb5fa005ec3c6635e5c65fa
hugo <hugo.contatrabalho@gmail.com>
2025-10-31 19:22:35 -0300

    first commit

---
+++ js/escolhas.js
@@ -0,0 +1,19 @@
+const massas = [
+    { id: 'bolo-chocolate', nome: 'Chocolate', preco: 3.00 },
+    { id: 'bolo-baunilha',  nome: 'Baunilha',  preco: 2.50 },
+    { id: 'bolo-red',       nome: 'Red Velvet',preco: 4.00 }
+];
+
+const recheios = [
+    { id: 'rch-brigadeiro', nome: 'Brigadeiro',    preco: 4.00 },
+    { id: 'rch-beijinho',   nome: 'Beijinho',      preco: 4.00 },
+    { id: 'rch-doceleite',  nome: 'Doce de Leite', preco: 3.50 },
+    { id: 'rch-ninho',      nome: 'Ninho',         preco: 5.00 }
+];
+
+const coberturas = [
+    { id: 'cob-ganache',   nome: 'Ganache',   preco: 4.00 },
+    { id: 'cob-chantilly', nome: 'Chantilly', preco: 3.00 },
+    { id: 'cob-granulado', nome: 'Granulado', preco: 1.00 },
+    { id: 'cob-morangos',  nome: 'Morangos',  preco: 5.00 }
+];
